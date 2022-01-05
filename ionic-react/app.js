b3a797dce0f4621446adea65f7455733bd3bde11
MinJoo-Park01 <supermp@hufs.ac.kr>
2022-01-05 15:26:52 +0900

    feat: reset button, result card

--- ionic-react/app.js
+++ ionic-react/app.js
@@ -1,6 +1,13 @@
-const calculateBtn = document.querySelector('ion-button');
+const calculateBtn = document.getElementById('calc-btn');
+const resetBtn = document.getElementById('reset-btn');
 const heightInput = document.getElementById('height-input');
 const weightInput = document.getElementById('weight-input');
+const resultArea = document.getElementById('result');
+
+const resetInputs = () =>{
+    heightInput.value='';
+    weightInput.value='';
+};
 
 const calculateBmi = () =>{
     const EnteredHeight = +heightInput.value; //+ : string => number
@@ -8,7 +15,22 @@ const calculateBmi = () =>{
 
     const bmi = EnteredWeight / (EnteredHeight * EnteredHeight);
 
-    console.log(bmi);
+    if(isNaN(bmi)){
+        alert('Not a number, please check inputs!');
+        return;
+    }
+
+    const resultElement = document.createElement('ion-card');
+    resultElement.innerHTML = `
+    <ion-card-content>
+    <h2>${bmi}</h2>
+    </ion-card-content>
+    `;
+    resultArea.innerHTML='';
+    resultArea.appendChild(resultElement);
 };
 
+
+
 calculateBtn.addEventListener('click', calculateBmi);
+resetBtn.addEventListener('click',resetInputs);
